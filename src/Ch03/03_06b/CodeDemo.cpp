71192c7c0c385c1f55dc14007a8e6197726f4d5b
harshghalsasi <31008064+harshghalsasi@users.noreply.github.com>
2026-04-24 12:17:40 +0000

    updated code

--- src/Ch03/03_06b/CodeDemo.cpp
+++ src/Ch03/03_06b/CodeDemo.cpp
@@ -2,6 +2,9 @@
 // Exercise 03_06
 // Pointers, by Eduardo Corpeño 
 
+// Pointers are not only used to store address but also to change the data at stored address
+
+
 #include <iostream>
 #include <string>
 
@@ -18,6 +21,27 @@ int main(){
     // ptr itself is a var stored at another mem location and it stores addr of a
     std::cout << "Address of ptr is: " << &ptr << std::endl;
     std::cout << "Data of a, accessed thru ptr is: " << *ptr << std::endl;
+
+    // pointer to a pointer
+    int x = 6;
+    int *p;         //pointer declaration
+    p = &x;
+
+    int **q;        //need to declare q as **(pointer to a ptr)
+    q = &p;
+
+    int ***r = &q;      //r is ptr to q, whch is a ptr to p, which is ptr to x
+
+    std::cout << "value at x: " << *p << std::endl;
+    std::cout << "value at p: " << *q << std::endl;
+    std::cout << "value at x, accessed via q: " << **q << std::endl;
+    std::cout << "value at x, accessed via r: " << ***r << std::endl;
+
+    ***r = 12;
+    std::cout << "value at x, changed via r: " << x << std::endl;
+
+    **q = *p + 2;
+    std::cout << "value at x, changed via q: " << **q << std::endl;
 
     std::cout << std::endl << std::endl;
     return (0);
